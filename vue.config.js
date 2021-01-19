edd48f6d1be1df5ca5540584c4726dfa98e810c8
Bankole <techybanky@gmail.com>
2021-01-19 06:47:34 +0100

    first deploy attempt

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production' ? '/webpieces/' : '/',
+  // publicPath: '',
+  baseUrl: '/projects/vue/webpieces/dist/',
 };
