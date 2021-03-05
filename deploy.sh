239c3555debf2eab680b0cdcb431989c6d7ae729
Bankole Esan <techybanky@gmail.com>
2021-03-05 20:50:09 +0100

    Added projects

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
+git config user.email "techybanky@gmail.com"
+git config user.name "Bankole Esan"
 git add -A
 git commit -m "deploy"
-git push -f git@github.com:Bankole2000/webpieces.git master:gh-pages
+git push -f https://github.com/Bankole2000/webpieces.git master:gh-pages
 cd -
